b47af9e83493db5a8d74f80d980b10caaf1ba416
Akarsh Tyagi <akarshtyagi1@gmail.com>
2020-07-24 12:29:31 +0530

    popups

--- avengers/script.js
+++ avengers/script.js
@@ -5,9 +5,10 @@ var body = $("body");
 var content = $("div#content");
 var popup = $("div.popup-box");
 var img = $("img.popup-img");
+var close = $("#close-icon");
 
 function checkAnswer(event){  
-   
+
    name = event.target.id;
    answer = document.querySelector("input."+name).value;
    console.log(name);      
@@ -17,6 +18,12 @@ function checkAnswer(event){
       img.addClass("animation");
       popup.addClass("visible");
    }
+
+   close.click(function () {
+      content.removeClass("blur");
+      img.removeClass("animation");
+      popup.removeClass("visible");
+     });
 
 }
 
